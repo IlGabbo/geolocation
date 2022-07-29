84fd1f6d67ad8acb84cd820c62d0fe208352c04b
IlGabbo <96393534+IlGabbo@users.noreply.github.com>
2022-07-29 11:03:18 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
 
+var x = document.getElementById("coordinates");
+navigator.geolocation.getCurrentPosition(showPosition);
+
+function showPosition(position) {
+   x.innerHTML = "Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude;
+}
